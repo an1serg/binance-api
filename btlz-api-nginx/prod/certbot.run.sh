b782940dec7e66c6ef94db752496436010873c75
hr82al <hr82al@gmail.com>
2024-07-10 09:12:30 +0300

    Deleted not needed folders

--- btlz-api-nginx/prod/certbot.run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker run -it --rm \
---name certbot \
--v ./certbot/conf/:/etc/letsencrypt/ \
--v ./certbot/www/:/var/www/certbot/ \
-certbot/certbot:latest \
-certonly \
---webroot --webroot-path /var/www/certbot/ \
--d btlz-api.ru -d www.btlz-api.ru
